6fd93c11ae6da60b4de6de03df6884b30dfe8eb4
Navin <navinkumar247@gmail.com>
2020-07-14 11:57:09 +0530

    Login Page and Index page styling are done, minor bug fixes

--- src/chat/views.py
+++ src/chat/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 from django.contrib.auth.decorators import login_required
-
+from profiles.models import Relationship
+@login_required
 def index(request):
 
     context = {
@@ -13,7 +14,21 @@ def index(request):
 
 @login_required
 def room(request, room_name):
+    
+    message_receiver = get_message_receiver(request, room_name)
+
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username': mark_safe(json.dumps(request.user.username)),
+        'friends': request.user.profile.get_friends(),
+        'message_receiver': message_receiver,
     })
+@login_required
+def get_message_receiver(request, room_name):
+    relation = Relationship.objects.get(slug = room_name)
+    
+    if relation.receiver.user != request.user:
+        message_receiver = relation.receiver
+    else:
+        message_receiver = relation.sender
+    return message_receiver
--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -4,14 +4,17 @@ from django import forms
 from .models import Profile
 
 class UserForm(UserCreationForm):
-    password1 = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(label= '', widget=forms.TextInput( attrs={'Placeholder' : 'Username',} ))
+    password1 = forms.CharField(label= '',  widget=forms.PasswordInput(attrs = {'Placeholder': 'Password',}))
+    password2 = forms.CharField(label= '',  widget=forms.PasswordInput(attrs = {'Placeholder':'Confirm Password',} ))
     class Meta:
         model = User
         fields = ('username', 'password1','password2')
 
 class ProfileForm(forms.ModelForm):
-    
+    email = forms.EmailField(label= '', widget=forms.TextInput(attrs = {'Placeholder': 'Email'}))
+    image = forms.ImageField(label= '', required=False, widget=forms.ClearableFileInput(attrs = {'Placeholder': 'Profile Picture', 'class':'image'}), )
+    bio  = forms.CharField(label= '', required=False,  widget=forms.Textarea(attrs = {'Placeholder': 'Tell something about yourself', 'rows':6,'cols':30}))
     class Meta:
         model = Profile 
         fields = ("email",'image','bio')
